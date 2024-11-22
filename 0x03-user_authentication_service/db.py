e425e96f6a1a709b282b3c3ef72d11479dca7cc5
“Onyinye-Judith” <onyinyejudith55@yahoo.com>
2024-11-22 05:14:54 +0100

    User-Authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,29 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pythonii
 """DB module
 """
 import logging
 from typing import Dict
 
-from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.orm.session import Session
-
-from user import Base, User
-
-logging.disable(logging.WARNING)
-
-
-class DB:
-        """DB class
-            """
-
-                def __init__(self) -> None:
-                            """Initialize a new DB instance
+def __init__(self) -> None:
+    """Initialize a new DB instance
                                     """
-                                            self._engine = create_engine("sqlite:///a.db", echo=True)
+                                    self._engine = create_engine("sqlite:///a.db", echo=True)
                                                     Base.metadata.drop_all(self._engine)
                                                             Base.metadata.create_all(self._engine)
                                                                     self.__session = None
