603cb11a2e35275c5762103d2d719543b1ec3479
Onyinye-Judith <onyinyejudith55@yahoo.com>
2024-11-29 21:25:56 +0100

    Personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = separator.join(f'({field})' for field in fields)
-    return re.sub(pattern, lambda match: separator.join(redaction if group else '' for group in match.groups()), message)
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+        pattern = separator.join(f'({field})' for field in fields)
+            return re.sub(pattern, lambda match: separator.join(redaction if group else '' for group in match.groups()), message)
